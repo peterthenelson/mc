d83aee8416535c09a39276776212bade4658dd48
peterthenelson <nelson.peter.a@gmail.com>
2017-09-26 04:21:18 +0000

    Improve debugging.

--- baxter/test_util.py
+++ baxter/test_util.py
@@ -1,6 +1,16 @@
 """Utilities for testing."""
 
+import functools
 import logging
+
+def debug(f):
+  """Decorator that turns on debug logging for the duration of the function."""
+  @functools.wraps(f)
+  def decorated_function(*args, **kwargs):
+    """Call f inside of DebugOn context manager."""
+    with DebugOn():
+      return f(*args, **kwargs)
+  return decorated_function
 
 class DebugOn(object):
   """Context handler to temporarily turn on debug logging."""
